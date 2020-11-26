8439408bf72a0c584d3d048fd013f0409b643e2c
MarioKobayashi <kobayashimariogo@yahoo.co.jp>
2020-11-26 22:49:14 +0900

    UFOの左右移動を制作

---
+++ Assets/Scripts/CameraCon.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//カメラ制御のコンポーネント
+public class CameraCon : MonoBehaviour
+{
+    public GameObject UFO;
+
+    void LateUpdate()
+    {
+        //UFOがいない場合処理は終了する
+        if (!UFO) return;
+
+        //UFOが存在する場合、UFOに追従する
+        //UFOのポジションをufoPosへ代入する
+        Vector3 ufoPos = this.UFO.transform.position;
+
+        //gameObject(カメラ)をUFOに追従させる
+        gameObject.transform.position = new Vector3(ufoPos.x, ufoPos.y, -10);
+    
+    }
+}
--- Assets/Scripts/UFO.cs
+++ Assets/Scripts/UFO.cs
@@ -5,15 +5,73 @@ using UnityEngine;
  // UFOのコンポーネント
 public class UFO : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //rigidbodyコンポーネントの変数
+    Rigidbody2D rigid2D;
+
+    //左右移動の力
+    public float moveForce;
+
+    //左右移動の最大の力
+    public float MaxmoveSpeed;
+
+    //左右移動のブレーキの力
+    public float friction;
+
+    //左右のキー入力を取得する変数
+    private int Key;
+
     void Start()
     {
-        
+        //rigidbody2Dコンポーネントを取得し、rigid2D変数へ代入
+        this.rigid2D = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        //矢印を押していない間はKeyを0にする
+        Key = 0;
+
+        //右矢印を押している間
+        if (Input.GetKey(KeyCode.RightArrow)) Key = 1;
+
+        //左矢印を押している間
+        if (Input.GetKey(KeyCode.LeftArrow)) Key = -1;
+
+       
     }
+
+    void FixedUpdate()
+    {
+
+        //Keyをそのまま使うとエラーが出るので下記の変数にKeyを代入
+        int direction_x = Key;
+
+        //UFOの左右移動の速度
+        float speedx = Mathf.Abs(this.rigid2D.velocity.x);
+        Debug.Log(speedx);
+
+        //左より右が大きい場合、
+        if (speedx < this.MaxmoveSpeed)
+        {
+            this.rigid2D.AddForce(transform.right * direction_x * this.moveForce);
+        }
+
+        //矢印キーを押していない&& +X方向へスピードが0より大きい場合、ブレーキをかける
+        if(direction_x == 0 && this.rigid2D.velocity.x > 0)
+        {
+            //-X方向へブレーキをかける
+            this.rigid2D.AddForce(transform.right * -this.friction);
+        }
+
+        //矢印キーを押していない && -X方向へスピードが0より大きい場合、ブレーキをかける
+        if (direction_x == 0 && this.rigid2D.velocity.x < 0)
+        {
+            //X方向へブレーキをかける
+            this.rigid2D.AddForce(transform.right * this.friction);
+        }
+
+    }
+
+
 }
