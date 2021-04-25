407587e4c8e4d7e77bc4b969838e785c21ff7137
MarioKobayashi <kobayashimariogo@yahoo.co.jp>
2021-04-25 22:53:53 +0900

    easysaveを入れる前にバックアップをとる

---
+++ Assets/Scripts/ItemDescriptionData.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Item_Des_Data", menuName = "Item_Des_Data")]
+public class ItemDescriptionData : ScriptableObject
+{
+    //アイテムの名前(配列)
+    public string missile = "- タップミサイル -";
+    public string bomb = "- スーパーボム -"; //仮
+    public string shild = "- タイムシールド -"; //仮
+    public string empty = "- ???? -";//まだアイテムを持っていない場合は????になる
+    //アイテムの名前
+
+
+    //アイテムの説明(配列)
+    public string missile_des = "タップした場所にミサイルを打つ事が出来るアタッチメント。\n \n約1秒に一発打ち込む事が可能だ。";
+    public string bomb_des = "UFOの真下に爆弾を落下させる事が出来るアタッチメント。\n \n 驚異の破壊力を誇る。";
+    public string shild_des = "発動から3秒間全ての攻撃を防ぐアタッチメント。\n \n　ピンチの時に使おう。";
+    public string empty_des = "????"; //アイテムを持っていない場合
+    //アイテムの説明(配列)
+
+
+
+
+
+
+
+
+}
---
+++ Assets/Scripts/UI/WakuController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WakuController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] waku = null;
+
+    private void OnEnable()
+    {
+        for (int i = 0; i < waku.Length; i++)
+        {
+            waku[i].SetActive(false); //オブジェクトが有効になったら枠を初期化
+        }
+    }
+
+    public void wakuNum(int num)//押されたボタンの枠が有効になり、押されていない枠は無効になる
+    {
+        for(int i = 0; i < waku.Length; i++)
+        {
+            if(i == num)
+            {
+                waku[i].SetActive(true);
+            }else if(i != num)
+            {
+                waku[i].SetActive(false);
+            }
+        }
+    }
+
+
+
+}
